df689f6be656e2341bc54257d1fda18dabd2f179
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-10-02 05:12:39 -0700

    pycode style fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" Script that registers a Blueprint and runs Flask """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -13,7 +15,8 @@ def close_storage(exception):
     """Closes the current SQLAlchemy session."""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+"""Index File"""
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def get_status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
