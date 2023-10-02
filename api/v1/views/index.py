b38e428a51449269f59a6bbd642a6672bc318478
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-10-02 04:45:37 -0700

    api running

---
+++ api/v1/views/index.py
@@ -0,0 +1,7 @@
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """Returns the status of the API."""
+    return jsonify({"status": "OK"})
