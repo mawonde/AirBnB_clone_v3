c7cf87cd2ddce5b119ef60930c36946b081f444e
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-10-03 13:05:22 -0700

    if statement

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,10 +32,10 @@ app.config['SWAGGER'] = {
 }
 
 
-swagger(app)
+Swagger(app)
 
 
-if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
